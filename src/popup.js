4a5137bd36abfe8e2c4695da267833f72e82a6fa
aynotna <aabelugin@gmail.com>
2025-03-01 03:35:25 +0300

    добавил запуск 2048

--- src/popup.js
+++ src/popup.js
@@ -10,9 +10,10 @@ document.getElementById("startTanks").addEventListener("click", function () {
 document.getElementById("startTTT").addEventListener("click", function () {
         window.open("ttt.html", "_blank");
 }); 
-document.getElementById("startPacman").addEventListener("click", function () {
-        window.open("Pacman/pacman.html", "_blank");
-});
 document.getElementById("start2048").addEventListener("click", function () {
         window.open("2048/2048.html", "_blank");
+});
+
+document.getElementById("startPacman").addEventListener("click", function () {
+        window.open("Pacman/pacman.html", "_blank");
 });
