3e87bcc099ff681b9615fa2278715a19d7cac39c
aynotna <aabelugin@gmail.com>
2025-03-01 03:28:34 +0300

    распределил все по попочкам

--- src/popup.js
+++ src/popup.js
@@ -1,15 +1,18 @@
 document.getElementById("startSnake").addEventListener("click", function () {
-      window.open("snake.html", "_blank");
+      window.open("Snake/snake.html", "_blank");
   });
 document.getElementById("startTetris").addEventListener("click", function () {
-        window.open("tetris.html", "_blank");
+        window.open("Tetris/tetris.html", "_blank");
 });
 document.getElementById("startTanks").addEventListener("click", function () {
         window.open("tanks.html", "_blank");
 });
 document.getElementById("startTTT").addEventListener("click", function () {
         window.open("ttt.html", "_blank");
-});
+}); 
 document.getElementById("startPacman").addEventListener("click", function () {
-        window.open("pacman.html", "_blank");
+        window.open("Pacman/pacman.html", "_blank");
+});
+document.getElementById("start2048").addEventListener("click", function () {
+        window.open("2048/2048.html", "_blank");
 });
