027b8e60d57a8416193165d48d1ebcae2eff6265
aynotna <aabelugin@gmail.com>
2025-03-03 18:29:01 +0300

    флеппиберд

--- src/popup.js
+++ src/popup.js
@@ -4,8 +4,8 @@ document.getElementById("startSnake").addEventListener("click", function () {
 document.getElementById("startTetris").addEventListener("click", function () {
         window.open("Tetris/tetris.html", "_blank");
 });
-document.getElementById("startTanks").addEventListener("click", function () {
-        window.open("tanks.html", "_blank");
+document.getElementById("startBox").addEventListener("click", function () {
+        window.open("FlappyBird/FlappyBird.html", "_blank");
 });
 document.getElementById("startTTT").addEventListener("click", function () {
         window.open("TTT/ttt.html", "_blank");
