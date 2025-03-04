134071f8766a800385e559a789529a9de8e8113d
sofiya <sofiya.fly@mail.ru>
2025-03-04 18:44:52 +0300

    Доработка

--- src/Main/main.js
+++ src/Main/main.js
@@ -1,10 +1,23 @@
-// Переключение темы
-function toggleTheme() {
-    document.body.classList.toggle('dark-mode');
-    localStorage.setItem('theme', document.body.classList.contains('dark-mode') ? 'dark' : 'light');
-}
+console.log("knbjsnbisnb");
 
-// Проверка сохранённой темы
-if (localStorage.getItem('theme') === 'dark') {
-    document.body.classList.add('dark-mode');
-}
+// Переключение темы
+document.addEventListener("DOMContentLoaded", () => {
+    const themeSwitch = document.getElementById("theme-switch");
+
+    // Функция переключения темы
+    function toggleTheme() {
+        document.body.classList.toggle("dark-mode");
+
+        // Сохранение состояния темы в localStorage
+        const isDark = document.body.classList.contains("dark-mode");
+        localStorage.setItem("theme", isDark ? "dark" : "light");
+    }
+
+    // Устанавливаем тему при загрузке страницы
+    if (localStorage.getItem("theme") === "dark") {
+        document.body.classList.add("dark-mode");
+    }
+
+    // Назначаем обработчик события
+    themeSwitch.addEventListener("click", toggleTheme);
+});
--- src/popup.js
+++ src/popup.js
@@ -1,25 +1,3 @@
 document.getElementById("Start_button").addEventListener("click", function () {
-        window.open("menu.html", "_blank");
+        window.open("/src/Main/main.html", "_blank");
     });
-
-
-document.getElementById("startSnake").addEventListener("click", function () {
-      window.open("Snake/snake.html", "_blank");
-  });
-document.getElementById("startTetris").addEventListener("click", function () {
-        window.open("Tetris/tetris.html", "_blank");
-});
-document.getElementById("startBox").addEventListener("click", function () {
-        window.open("FlappyBird/FlappyBird.html", "_blank");
-});
-document.getElementById("startTTT").addEventListener("click", function () {
-        window.open("TTT/ttt.html", "_blank");
-}); 
-document.getElementById("start2048").addEventListener("click", function () {
-        window.open("2048/2048.html", "_blank");
-});
-
-document.getElementById("startPacman").addEventListener("click", function () {
-        window.open("Pacman/pacman.html", "_blank");
-});
-
