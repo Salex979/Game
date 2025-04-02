7e0f2fde41971fedf7e3fb9a979239e1f72611e6
Yaroslav <yaroslav.naumov1000-7@yandex.ru>
2025-04-02 03:28:12 +0300

    Испарвлена ошибка с регистрацией!

--- src/popup.js
+++ src/popup.js
@@ -16,12 +16,29 @@ document.addEventListener("DOMContentLoaded", function () {
             const data = await response.json();
 
             if (response.ok) {
-                // Сохраняем в chrome.storage, что пользователь только что вошел
                 chrome.storage.local.set({ token: data.token, showWelcome: true });
-
-                // Переход на главную страницу
                 chrome.tabs.create({ url: "/src/Main/main.html" });
+            } else {
+                document.getElementById("status").textContent = "Ошибка: " + data.message;
+            }
+        });
+    }
 
+    if (registerBtn) {
+        registerBtn.addEventListener("click", async () => {
+            const username = document.getElementById("username").value;
+            const password = document.getElementById("password").value;
+
+            const response = await fetch("http://localhost:3000/api/register", {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ username, password })
+            });
+
+            const data = await response.json();
+
+            if (response.ok) {
+                document.getElementById("status").textContent = "Регистрация успешна! Теперь войдите.";
             } else {
                 document.getElementById("status").textContent = "Ошибка: " + data.message;
             }
